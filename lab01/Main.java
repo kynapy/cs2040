c3a644afb7a221b6497e20a7bc71d9ca39d023bf
Kiyan Aftershock <apyoung11@hotmail.com>
2022-02-09 12:14:15 +0800

    lab01

---
+++ lab01/Main.java
@@ -0,0 +1,37 @@
+// Ang Ping Young (A0199498X)
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String arg[]) {
+        Scanner sc = new Scanner(System.in);
+        int restaurants = sc.nextInt();
+        String result = "Anywhere is fine I guess";
+        boolean found = false;
+
+        for (int i = 0; i < restaurants; i++) {
+            int itemsOnMenu = sc.nextInt();
+            String x = sc.nextLine();
+            String restaurantName = sc.nextLine();
+
+            boolean pancakes = false;
+            boolean peasoup = false;
+
+            for (int j = 0; j < itemsOnMenu; j++) {
+                String item = sc.nextLine();
+                if (item.equals("pancakes")) {
+                    pancakes = true;
+                }
+                if (item.equals("pea soup")) {
+                    peasoup = true;
+                }
+                if (peasoup && pancakes) {
+                    if (!found) {
+                        result = restaurantName;
+                        found = true;
+                    }
+                }
+            }
+        }
+        System.out.println(result);
+    }
+}
