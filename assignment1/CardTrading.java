d7f970c7930dc38e0c587f4419836e2653b46b73
Kiyan Aftershock <apyoung11@hotmail.com>
2022-02-14 10:56:58 +0800

    assignment1b 80% done

--- assignment1/CardTrading.java
+++ assignment1/CardTrading.java
@@ -11,45 +11,97 @@ import java.util.Collections;
 
 public class CardTrading {
     public static void main(String arg[]) throws Exception {
+        //Reading and writing classes
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+        
+        // First line of inputs
         String[] requirements = br.readLine().split(" ");
-        String[] anthonysCards = br.readLine().split(" ");
-        Arrays.sort(anthonysCards);
         int numberOfCards = Integer.parseInt(requirements[0]);
         int typesOfCards = Integer.parseInt(requirements[1]);
-        ArrayList<Card> cards = new ArrayList<Card>();
+        int requiredCombos = Integer.parseInt(requirements[2]);
 
-        // Adds the types of carry to ArrayList cards, can be accessed by indexing
-        for (int i = 1; i <= typesOfCards; i++) {
+        // Stores Anthony's cards in an array
+        String[] cardsOwned = br.readLine().split(" ");
+
+        // Adds the types of cards to ArrayList cards, can be accessed by indexing  by (value - 1)
+        ArrayList<Pair> possibleCards = new ArrayList<Pair>();
+        for (int i = 0; i < typesOfCards; i++) {
             String[] card = br.readLine().split(" ");
-            cards.add(new Card(i, Integer.parseInt(card[0]), Integer.parseInt(card[1]))); 
+            possibleCards.add(new Pair(Long.parseLong(card[0]), Long.parseLong(card[1])));
         }
+
+        // Counts the number of cards Anthony has and stores the data in a DAT
+        int[] cardsPerType = new int[typesOfCards];
+        for (int i = 0; i < numberOfCards; i++) {
+            int number = Integer.parseInt(cardsOwned[i]);
+            cardsPerType[number-1]+=1;
+        }
+
+        // Calculate the cost of getting a combo for each card and sort them by the cost
+        ArrayList<Card> cardCost = new ArrayList<Card>();
+        for (int i = 0; i < typesOfCards; i++) {
+            long buyPrice = possibleCards.get(i).buyPrice;
+            long sellPrice = possibleCards.get(i).sellPrice;
+            int ownedCopies = cardsPerType[i];
+            long cost = 0;
+            if (ownedCopies == 0) {
+                cost = 2 * buyPrice;
+            } 
+            else if (ownedCopies == 1) {
+                cost = buyPrice + sellPrice;
+            } else {
+                cost = 2 * sellPrice;
+            }
+            cardCost.add(new Card(i+1, cost));
+        }
+        Collections.sort(cardCost);
+
+        // Calculate profit/loss
+        long result = 0;
+        
 
         // Testing 
-        for (int i = 0; i < numberOfCards; i++) {
-            pw.println(anthonysCards[i]);
+        for (int i=0; i<typesOfCards; i++) {
+            System.out.println(cardCost.get(i).cost + " " + cardCost.get(i).value);
         }
-        for (int i = 0; i < typesOfCards; i++) {
-            pw.println(cards.get(i));
-        }
+
+        // Close PrintWriter, end of program
         pw.close();
     }
 }
 
-class Card {
-    private final int number;
-    private final int buyPrice;
-    private final int sellPrice;
+class Pair {
+    public final long buyPrice;
+    public final long sellPrice;
 
-    Card(int number, int buyPrice, int sellPrice) {
-        this.number = number;
+    Pair(long buyPrice, long sellPrice) {
         this.buyPrice = buyPrice;
         this.sellPrice = sellPrice;
     }
 
     @Override
     public String toString() {
-        return String.format("Card %d, buying at %d, selling at %d", number, buyPrice, sellPrice);
+        return String.format("Buying at %d, selling at %d", buyPrice, sellPrice);
+    }
+}
+
+class Card implements Comparable<Card> {
+    public final int value;
+    public final long cost;
+
+    Card(int value, long cost) {
+        this.value = value;
+        this.cost = cost;
+    }
+
+    @Override
+    public int compareTo(Card other) {
+        long x = this.cost - other.cost;
+        if (x < 0) {
+            return -1;
+        } else {
+            return 1;
+        }
     }
 }
