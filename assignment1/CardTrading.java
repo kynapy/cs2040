d5a5163039e68d9e9d14875fe7783e86f49083a2
Kiyan Aftershock <apyoung11@hotmail.com>
2022-02-15 00:32:36 +0800

    increased speed and added test cases

--- assignment1/CardTrading.java
+++ assignment1/CardTrading.java
@@ -24,14 +24,6 @@ public class CardTrading {
         // Stores Anthony's cards in an array
         String[] cardsOwned = br.readLine().split(" ");
 
-        // Adds the types of cards to ArrayList cards, can be accessed by indexing by value (correct)
-        ArrayList<Pair> possibleCards = new ArrayList<Pair>();
-        possibleCards.add(0 , new Pair(0,0));
-        for (int i = 1; i <= typesOfCards; i++) {
-            String[] card = br.readLine().split(" ");
-            possibleCards.add(i, new Pair(Long.parseLong(card[0]), Long.parseLong(card[1])));
-        }
-
         // Counts the number of cards Anthony has and stores the data in a DAT (Correct)
         int[] cardsPerType = new int[typesOfCards+1];
         for (int i = 0; i < numberOfCards; i++) {
@@ -42,8 +34,9 @@ public class CardTrading {
         // Calculate the cost of getting a combo for each card and sort them by the cost (Correct)
         ArrayList<Card> cardCost = new ArrayList<Card>();
         for (int i = 1; i <= typesOfCards; i++) {
-            long buyPrice = possibleCards.get(i).buyPrice;
-            long sellPrice = possibleCards.get(i).sellPrice;
+            String[] prices = br.readLine().split(" ");
+            long buyPrice = Long.parseLong(prices[0]);
+            long sellPrice = Long.parseLong(prices[1]);
             int ownedCopies = cardsPerType[i];
             long cost = 0;
             if (ownedCopies == 0) {
@@ -54,28 +47,17 @@ public class CardTrading {
             } else {
                 cost = 2 * sellPrice;
             }
-            cardCost.add(new Card(i, cost));
+            cardCost.add(new Card(i, buyPrice, sellPrice, cost));
         }
         Collections.sort(cardCost);
 
-        // Adding required cards for a proper deck (Correct)
-        ArrayList<Integer> requiredCards = new ArrayList<Integer>();
-        for (int i = 0; i < requiredCombos; i++) {
-            requiredCards.add(cardCost.get(i).value);
-        }
-
         // Selling and buying cards
         long result = 0;
-        
-        for (int i = 1; i < typesOfCards+1; i++) {
-            if (requiredCards.contains(i)) { // We want to buy until we hit 2 cards
-                if (cardsPerType[i] != 2) {
-                    result-= (2-cardsPerType[i]) * (possibleCards.get(i).buyPrice);
-                }
+        for (int i = 0; i < typesOfCards; i++) {
+            if (i < requiredCombos) {
+                result -= (2 - cardsPerType[i+1]) * cardCost.get(i).buyPrice;
             } else {
-                if (cardsPerType[i] != 0) { // Selling all cards
-                    result+= (cardsPerType[i]) * possibleCards.get(i).sellPrice;
-                }
+                result += cardsPerType[i+1] * cardCost.get(i).sellPrice;
             }
         }
 
@@ -87,32 +69,29 @@ public class CardTrading {
     }
 }
 
-class Pair {
-    public final long buyPrice;
-    public final long sellPrice;
-
-    Pair(long buyPrice, long sellPrice) {
-        this.buyPrice = buyPrice;
-        this.sellPrice = sellPrice;
-    }
-}
-
 class Card implements Comparable<Card> {
     public final int value;
-    public final long cost;
+    public final long buyPrice;
+    public final long sellPrice;
+    public final long costOfOwning;
 
-    Card(int value, long cost) {
+    Card(int value, long buyPrice, long sellPrice, long costOfOwning) {
         this.value = value;
-        this.cost = cost;
+        this.buyPrice = buyPrice;
+        this.sellPrice = sellPrice;
+        this.costOfOwning = costOfOwning;
     }
 
     @Override
     public int compareTo(Card other) {
-        long x = this.cost - other.cost;
+        long x = this.costOfOwning - other.costOfOwning;
         if (x < 0) {
             return -1;
+        } 
+        else if (x > 0) {
+            return 1;    
         } else {
-            return 1;
+            return 0;
         }
     }
 }
