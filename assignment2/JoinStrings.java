94cb8704a6673bf43f52cc2cf76c41dbf766ed11
Kiyan Aftershock <apyoung11@hotmail.com>
2022-02-16 01:13:55 +0800

    backbone for assignment 1a

---
+++ assignment2/JoinStrings.java
@@ -0,0 +1,21 @@
+// Ang Ping Young (A0199498X)
+// Kattis Best: 0., My Timing: 0. 
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class JoinStrings {
+    public static void main(String arg[]) throws Exception {
+        //Reading and writing classes
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+        
+        // Main code
+
+    }
+}
