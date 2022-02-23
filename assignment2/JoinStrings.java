a70b3f30ca3bb08a4a5ac7e966599842ded614ee
Kiyan Aftershock <apyoung11@hotmail.com>
2022-02-24 03:41:41 +0800

    assignment 2a done

--- assignment2/JoinStrings.java
+++ assignment2/JoinStrings.java
@@ -1,13 +1,11 @@
 // Ang Ping Young (A0199498X)
-// Kattis Best: 0., My Timing: 0. 
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.BufferedWriter;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.HashMap;
 
 public class JoinStrings {
     public static void main(String arg[]) throws Exception {
@@ -16,6 +14,59 @@ public class JoinStrings {
         PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
         
         // Main code
+        // Reading the string inputs
+        int numOfInputs = Integer.parseInt(br.readLine());
+        LinkedStrings inputs[] = new LinkedStrings[numOfInputs + 1];
+        for (int i = 1; i < numOfInputs+1; i++) {
+            LinkedStrings input = new LinkedStrings(br.readLine());
+            inputs[i] = input;
+        }
 
+        // Linking up the strings
+        int start = 1;
+        HashMap<Integer, Integer> mapping = new HashMap<Integer, Integer>(); 
+        for (int i = 0; i < numOfInputs - 1; i++) {
+            String[] input = br.readLine().split(" ");
+            int front = Integer.parseInt(input[0]);
+            int back = Integer.parseInt(input[1]);
+            int frontCopy = front;
+            if (mapping.containsKey(front)) {
+                front = mapping.get(front);
+                if (mapping.containsKey(back)) {
+                    mapping.put(frontCopy, mapping.get(back));
+                } else {
+                    mapping.put(frontCopy, back);
+                }
+            } else {
+                if (mapping.containsKey(back)) {
+                    mapping.put(front, mapping.get(back));
+                } else {
+                    mapping.put(front, back);
+                }
+            }
+            inputs[front].tail = inputs[back];
+            if (i == (numOfInputs-2)) {
+                start = frontCopy;
+            }
+        }
+
+        // Joining everything together
+        LinkedStrings pointer = inputs[start];
+        for (int i = 0; i < numOfInputs; i++) {
+            pw.print(pointer.words);
+            pointer = pointer.tail;
+        }
+        pw.println();
+        pw.close();
+    }
+}
+
+class LinkedStrings {
+    public final String words;
+    public LinkedStrings tail;
+
+    LinkedStrings(String words) {
+        this.words = words;
+        this.tail = null;
     }
 }
