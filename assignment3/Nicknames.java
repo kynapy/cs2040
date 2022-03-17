6f4bda5d3b57475f5ac16c653067edee98e76533
Kiyan Aftershock <apyoung11@hotmail.com>
2022-03-17 19:10:18 +0800

    skeleton code for assignment 3

---
+++ assignment3/Nicknames.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+
+public class Nicknames {
+    public static void main(String arg[]) throws Exception {
+        //Reading and writing classes
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+        
+        // Main code
+    }
+}
