3d7d017764af38966aba270458845f38ac5318d7
Kiyan Aftershock <apyoung11@hotmail.com>
2022-03-27 01:16:42 +0800

    terrible TC but assignment3a done, redo when free

--- assignment3/AlmostUnionFind.java
+++ assignment3/AlmostUnionFind.java
@@ -1,4 +1,5 @@
-// Ang Ping Young (A0199498X)
+// Kattis ID: almostunionfind
+// Time taken: 3.50s, Fastest time: 0.38s
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -14,7 +15,6 @@ public class AlmostUnionFind {
         PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
 
         // Main code
-
         while (true) {
             // Reading and processing first line
             String input = br.readLine();
@@ -33,14 +33,10 @@ public class AlmostUnionFind {
                 String[] command = br.readLine().split(" ");
                 String operation = command[0];
                 if (operation.equals("1")) {
-                    int set1 = Integer.parseInt(command[1]);
-                    int set2 = Integer.parseInt(command[2]);
-                    numbers.unionSet(set1, set2);
+                    numbers.unionSet(Integer.parseInt(command[1]), Integer.parseInt(command[2]));
                 }
                 else if (operation.equals("2")) {
-                    int set1 = Integer.parseInt(command[1]);
-                    int set2 = Integer.parseInt(command[2]);
-                    numbers.moveSet(set1, set2);
+                    numbers.moveSet(Integer.parseInt(command[1]), Integer.parseInt(command[2]));
                 } else {
                     int set = Integer.parseInt(command[1]);
                     pw.println(numbers.setSize(set) + " " + numbers.sumSet(set));
@@ -55,30 +51,32 @@ class UnionFind {
     public int p[];
     public HashSet<Integer> capos[];
     public long sum[];
+    public int size[];
 
     @SuppressWarnings("unchecked")
     UnionFind(int N) {
         p = new int[N];
         capos = new HashSet[N];
         sum = new long[N];
+        size = new int[N];
         for (int i = 0; i < N; i++) {
             p[i] = i;
             capos[i] = new HashSet<Integer>();
             sum[i] = i;
+            size[i] = 1;
         }
     }
 
     // Implementation for Problem
-    public void unionSet(int x, int y) { // O(1)
+    public void unionSet(int x, int y) { // O(1) ish
         if (isSameSet(x,y)) {
             return;
         } else {
             int newDon = findSet(y);
             int oldDon = findSet(x);
-            capos[newDon].add(oldDon);
-            capos[newDon].addAll(capos[oldDon]);
-            p[oldDon] = newDon;
-            capos[oldDon].clear();
+            capos[y].add(oldDon);
+            p[oldDon] = y;
+            size[newDon] += size[oldDon];
             sum[newDon] += sum[oldDon];
         }
     }
@@ -89,13 +87,16 @@ class UnionFind {
         } else {
             if (x == findSet(x)) { // x is don of set
                 int newDon = findSet(y);
-                capos[newDon].add(x);
                 sum[newDon] += x;
+                size[newDon] += 1;
+                capos[y].add(x);
                 if (!capos[x].isEmpty()) {
                     Integer[] transfer = capos[x].toArray(new Integer[capos[x].size()]);
                     int replacementDon = transfer[0];
                     sum[replacementDon] = sum[x]-x;
                     sum[x] = x;
+                    size[replacementDon] = size[x] - 1;
+                    size[x] = 1;
                     for (int i = 0; i < transfer.length; i++) {
                         p[transfer[i]] = replacementDon;
                         if (replacementDon != transfer[i]) {
@@ -103,19 +104,24 @@ class UnionFind {
                         }
                     }
                 }
-                capos[x].clear();
-                p[x] = newDon;
+                capos[x] = new HashSet<Integer>();
+                p[x] = y;
             } else { // x is a capo
                 int newDon = findSet(y);
                 int oldDon = findSet(x);
-                capos[oldDon].remove(x);
+                int oneUp = p[x];
+                capos[oneUp].remove(x);
                 sum[oldDon] -= x;
-                for (Integer capo: capos[oldDon]) {
-                    p[capo] = oldDon;
+                size[oldDon] -= 1;
+                for (Integer capo: capos[x]) {
+                    p[capo] = oneUp;
+                    capos[oneUp].add(capo);
                 }
-                capos[newDon].add(x);
-                p[x] = newDon;
+                capos[x] = new HashSet<Integer>();
+                capos[y].add(x);
+                p[x] = y;
                 sum[newDon] += x;
+                size[newDon] += 1;
             }
         }
     }
@@ -124,8 +130,8 @@ class UnionFind {
         return sum[findSet(x)];
     }
 
-    public int setSize(int x) { // O(1) time
-        return capos[findSet(x)].size() + 1;
+    public int setSize(int x) { // O(1) time-ish
+        return size[findSet(x)];
     }
 
     // Useful additional functions
