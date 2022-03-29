d606efdf4574bf3ca5c774c3622292981deedc4c
Kiyan Aftershock <apyoung11@hotmail.com>
2022-03-29 18:56:36 +0800

    assignment 4 skeleton code

---
+++ assignment4/MillionaireMadness.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+
+public class MillionaireMadness {
+    public static void main(String arg[]) throws Exception {
+        //Reading and writing classes
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+        
+        // Main code
+
+        pw.close();
+    }
+}
